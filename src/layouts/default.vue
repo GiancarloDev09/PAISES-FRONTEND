dac4543169190ce58ceaa5f5dac5e5299fc670fd
Giancarlo Joel Alvarez Gallardo <giancarloag09@gmail.com>
2024-10-29 21:57:58 -0500

    Subida de opcion de visualizar pais

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,99 +1,66 @@
 <template>
   <v-app>
-  <v-layout>
-    <v-navigation-drawer v-model="drawer" class="bg-deep-grey"theme="dark">
-      <v-container class="text-center pa-4">
+    <v-layout>
+      <v-navigation-drawer v-model="drawer" class="bg-deep-grey" theme="dark">
+        <v-container class="text-center pa-4">
           <v-img
-          ax-width="160"
-          style="max-width: 100%;height: auto;filter: invert(1) brightness(2);"
-          src="https://static.wixstatic.com/media/96f38e_00bc8de829464068a01361d80c000a67~mv2.png/v1/fill/w_152,h_32,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20Negro.png" max-width="120" contain />
-
+            ax-width="160"
+            style="max-width: 100%; height: auto; filter: invert(1) brightness(2);"
+            src="https://static.wixstatic.com/media/96f38e_00bc8de829464068a01361d80c000a67~mv2.png/v1/fill/w_152,h_32,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20Negro.png"
+            max-width="120"
+            contain
+          />
         </v-container>
-      <v-list density="compact" item-props :items="items" nav />
-
-      <template v-slot:append>
+        <v-list density="compact" item-props :items="items" nav />
+        <template v-slot:append>
           <div class="pa-2">
-            <v-btn block style="background-color: aliceblue;color: black;">
-              Cerrar Sesion
+            <v-btn block style="background-color: aliceblue; color: black;">
+              Cerrar Sesión
             </v-btn>
           </div>
         </template>
-    </v-navigation-drawer>
+      </v-navigation-drawer>
 
-    <v-app-bar border="b" class="ps-4" flat >
-      <v-btn v-if="$vuetify.display.mdAndUp" icon @click="drawer = !drawer">
+      <v-app-bar border="b" class="ps-4" flat>
+        <v-btn v-if="$vuetify.display.mdAndUp" icon @click="drawer = !drawer">
           <v-icon>mdi-menu</v-icon>
         </v-btn>
+        <v-app-bar-nav-icon v-if="$vuetify.display.smAndDown" @click="drawer = !drawer" />
+        <v-app-bar-title>Aplicación Países</v-app-bar-title>
+        <v-switch v-model="model" hide-details inset></v-switch>
+        <template #append>
+          <v-btn class="text-none me-2" height="48" icon slim>
+            <v-avatar color="surface-light" image="https://cdn-icons-png.flaticon.com/512/219/219986.png" size="32" />
+            <v-menu activator="parent">
+              <v-list density="compact" nav>
+                <v-list-item append-icon="mdi-cog-outline" link title="Configuraciones" to="/vista-configuraciones" />
+                <v-list-item append-icon="mdi-logout" link title="Cerrar Sesión" />
+              </v-list>
+            </v-menu>
+          </v-btn>
+        </template>
+      </v-app-bar>
 
-      <v-app-bar-nav-icon v-if="$vuetify.display.smAndDown" @click="drawer = !drawer" />
-
-      <v-app-bar-title>Aplicacion Paises</v-app-bar-title>
-      <v-switch
-        v-model="model"
-        hide-details
-        inset
-      ></v-switch>
-      <template #append>
-        <v-btn class="text-none me-2" height="48" icon slim>
-          <v-avatar color="surface-light" image="https://cdn-icons-png.flaticon.com/512/219/219986.png" size="32" />
-
-          <v-menu activator="parent">
-            <v-list density="compact" nav>
-              <v-list-item append-icon="mdi-cog-outline" link title="Configuraciones" to="/vista-configuraciones"/>
-
-              <v-list-item append-icon="mdi-logout" link title="Cerrar Sesion" />
-            </v-list>
-          </v-menu>
-        </v-btn>
-      </template>
-    </v-app-bar>
-
-    <v-main style="background-color:#90CAF9;">
-      <div class="pa-4">
-        <v-sheet
-          border="dashed md"
-          style="background-color:#90CAF9;"
-          height="auto"
-          rounded="lg"
-          width="100%"
-        >
-        <router-view />
-      </v-sheet>
-      </div>
-    </v-main>
-  </v-layout>
-</v-app>
+      <v-main style="background-color:#90CAF9;">
+        <div class="pa-4">
+          <v-sheet border="dashed md" style="background-color:#90CAF9;" height="auto" rounded="lg" width="100%">
+            <router-view />
+          </v-sheet>
+        </div>
+      </v-main>
+    </v-layout>
+  </v-app>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+import { ref } from 'vue'
 
-  const drawer = ref(true)
-  const model =ref(true)
-  const items = ref([
-    {
-      title: 'Home',
-      prependIcon: 'mdi-home',
-      link: true,
-      to:'/'
-    },
-    {
-      title: 'Vista Uno',
-      prependIcon: 'mdi-view-grid',
-      link: true,
-      to:'/vista-uno'
-    },
-    {
-      title: 'Vista dos',
-      prependIcon: 'mdi-archive-eye',
-      link: true,
-      to:'/vista-dos'
-    },
-    {
-      title: 'Configuraciones',
-      prependIcon: 'mdi-cog-outline',
-      link: true,
-      to:'/vista-configuraciones'
-    },
-  ])
+const drawer = ref(true)
+const model = ref(true)
+const items = ref([
+  { title: 'Home', prependIcon: 'mdi-home', link: true, to: '/' },
+  { title: 'Vista Uno', prependIcon: 'mdi-view-grid', link: true, to: '/vista-uno' },
+  { title: 'Vista dos', prependIcon: 'mdi-archive-eye', link: true, to: '/vista-dos' },
+  { title: 'Configuraciones', prependIcon: 'mdi-cog-outline', link: true, to: '/vista-configuraciones' },
+])
 </script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row cols="5" md="6">
-      <v-col >
+      <v-col>
         <v-text-field
           v-model="searchQuery"
           label="Buscar país"
@@ -37,14 +37,8 @@
         md="4"
         lg="3"
       >
-        <v-card class="mx-auto my-2" max-width="250">
-          <v-img
-            :src="country.imageUrl || country.flagUrl"
-            :alt="country.name"
-            height="100px"
-            cover
-          ></v-img>
-
+        <v-card class="mx-auto my-2" max-width="250" @click="selectCountry(country)">
+          <v-img :src="country.imageUrl || country.flagUrl" :alt="country.name" height="100px" cover></v-img>
           <v-card-item>
             <v-card-title class="text-subtitle-1">{{ country.name }}</v-card-title>
             <v-card-subtitle class="text-caption">{{ country.continent.name }}</v-card-subtitle>
@@ -55,20 +49,39 @@
     <div class="text-center mt-4">
       <v-pagination v-model="currentPage" :length="Math.ceil(countries.length / itemsPerPage)" />
     </div>
+
+    <!-- Modal para mostrar detalles del país -->
+    <v-dialog v-model="dialog" max-width="600px">
+      <v-card>
+        <v-card-title>{{ selectedCountry.name }}</v-card-title>
+        <v-card-subtitle>{{ selectedCountry.continent.name }}</v-card-subtitle>
+        <v-card-text>
+          <p><strong>Capital:</strong> {{ selectedCountry.capital }}</p>
+          <p><strong>Idioma:</strong> {{ selectedCountry.languages.map(lang => lang.name).join(', ') }}</p>
+          <p><strong>Moneda:</strong> {{ selectedCountry.currency }}</p>
+          <p><strong>Estados:</strong> {{ selectedCountry.states.map(state => state.name).join(', ') }}</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" text @click="dialog = false">Cerrar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
+
 <script setup>
-import axios from 'axios';
-import { ref, onMounted } from 'vue';
+import axios from 'axios'
+import { ref, onMounted, computed } from 'vue'
 
-const countries = ref([]);
-const unsplashAccessKey = 'rEY13Kw7mWyJq1sUueOfAC7IGJmWq8i3HFkA02bE'; // Reemplaza con tu Access Key
-const currentPage = ref(1);
-const itemsPerPage = 8;
-const searchQuery = ref('');
-const filterContinent = ref(null);
-const showContinents = ref(false);
-
+const countries = ref([])
+const unsplashAccessKey = 'rEY13Kw7mWyJq1sUueOfAC7IGJmWq8i3HFkA02bE' // Reemplaza con tu Access Key
+const currentPage = ref(1)
+const itemsPerPage = 8
+const searchQuery = ref('')
+const filterContinent = ref(null)
+const showContinents = ref(false)
+const dialog = ref(false)
+const selectedCountry = ref({})
 
 const getCountryImage = async (countryName) => {
   try {
@@ -77,41 +90,47 @@ const getCountryImage = async (countryName) => {
         client_id: unsplashAccessKey,
         query: countryName,
         per_page: 1,
-      }
-    });
+      },
+    })
     if (response.data.results.length > 0) {
-      return response.data.results[0].urls.regular;
+      return response.data.results[0].urls.regular
     } else {
-      return null;
+      return null
     }
   } catch (error) {
-    console.error(error);
-    return null;
+    console.error(error)
+    return null
   }
-};
+}
+
 const continents = computed(() => {
-  const continentSet = new Set();
-  countries.value.forEach(country => continentSet.add(country.continent.name));
-  return Array.from(continentSet);
-});
+  const continentSet = new Set()
+  countries.value.forEach((country) => continentSet.add(country.continent.name))
+  return Array.from(continentSet)
+})
 
 const filteredCountries = computed(() => {
-  let filtered = countries.value;
+  let filtered = countries.value
   if (searchQuery.value) {
-    const query = searchQuery.value.toLowerCase();
-    filtered = filtered.filter(country => country.name.toLowerCase().includes(query));
+    const query = searchQuery.value.toLowerCase()
+    filtered = filtered.filter((country) => country.name.toLowerCase().includes(query))
   }
   if (filterContinent.value) {
-    filtered = filtered.filter(country => country.continent.name === filterContinent.value);
+    filtered = filtered.filter((country) => country.continent.name === filterContinent.value)
   }
-  return filtered;
-});
+  return filtered
+})
 
 const paginatedCountries = computed(() => {
-  const startIndex = (currentPage.value - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  return filteredCountries.value.slice(startIndex, endIndex);
-});
+  const startIndex = (currentPage.value - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  return filteredCountries.value.slice(startIndex, endIndex)
+})
+
+const selectCountry = (country) => {
+  selectedCountry.value = country
+  dialog.value = true
+}
 
 onMounted(async () => {
   try {
@@ -124,28 +143,36 @@ onMounted(async () => {
             continent {
               name
             }
-            emoji
+            capital
+            languages {
+              name
+            }
+            currency
+            states {
+              name
+            }
           }
         }
-      `
-    });
+      `,
+    })
 
-    const countriesData = response.data.data.countries;
+    const countriesData = response.data.data.countries
 
     // Obtener imágenes y banderas en paralelo
-    await Promise.all(countriesData.map(async (country) => {
-      country.imageUrl = await getCountryImage(country.name);
-      // Si no hay imagen de Unsplash, usar la bandera
-      if (!country.imageUrl) {
-        const flagResponse = await axios.get(`https://restcountries.com/v3.1/alpha/${country.code}`);
-        country.flagUrl = flagResponse.data[0].flags.png;
-      }
-    }));
+    await Promise.all(
+      countriesData.map(async (country) => {
+        country.imageUrl = await getCountryImage(country.name)
+        // Si no hay imagen de Unsplash, usar la bandera
+        if (!country.imageUrl) {
+          const flagResponse = await axios.get(`https://restcountries.com/v3.1/alpha/${country.code}`)
+          country.flagUrl = flagResponse.data[0].flags.png
+        }
+      })
+    )
 
-    countries.value = countriesData;
-
+    countries.value = countriesData
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-});
+})
 </script>
